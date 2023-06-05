f21aa4cf8a8349edcb10fede172cdad37a299ac5
ALICE ADA AWUOR <aliceada96@gmail.com>
2023-06-06 00:16:31 +0300

    check cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,5 +9,22 @@
 
 int check_cycle(listint_t *list)
 {
+listint_t *i, *j;
 
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	i = list->next;
+	j = list->next->next;
+
+	while (i && j && j->next)
+	{
+		if (i == j)
+			return (1);
+
+		i = i->next;
+		hare = j->next->next;
+	}
+
+	return (0);
 }
