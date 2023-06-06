7b737f0590681b8ebece7b315fc329f9259dee22
ALICE ADA AWUOR <aliceada96@gmail.com>
2023-06-06 21:17:04 +0300

    islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
-            c = chr(ord(c) - 32)
-        print("{}".format(c), end="")
-    print("")
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
