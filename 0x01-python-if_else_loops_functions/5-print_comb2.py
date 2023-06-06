626fa0901fda6695b3c806d4b6ac2d8a0941607e
ALICE ADA AWUOR <aliceada96@gmail.com>
2023-06-06 20:19:30 +0300

    print in a line

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for number in range(0, 100):
     if number == 99:
         print("{}\n".format(number))
     else:
-        print("{:02}".format(number))
+        print("{:02}".format(number), end=", ")
