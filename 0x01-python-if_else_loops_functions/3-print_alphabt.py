5555d94f152f0f512da79271cedb4a0f4dcce5d3
ALICE ADA AWUOR <aliceada96@gmail.com>
2023-06-06 19:44:58 +0300

    print alpha mnus q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-    if str(letter) is not 'q' and str(letter) is not 'e':
-        print("{}".format(str(letter)), end="")
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
