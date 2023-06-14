767aab83268a2d5652ab32bb35569f2d58ab8308
ALICE ADA AWUOR <aliceada96@gmail.com>
2023-06-14 22:27:58 +0300

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -24,5 +24,6 @@ def roman_to_int(roman_string):
             num += roman_dict[roman_string[i]] * -1
 
         else:
-        num += roman_dict[roman_string[i]]
+            num += roman_dict[roman_string[i]]
+
     return (num)
