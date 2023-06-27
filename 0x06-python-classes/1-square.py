f6d65371173795281b1bd9261099e148fc064aac
ALICE ADA AWUOR <aliceada96@gmail.com>
2023-06-28 00:27:25 +0300

    initialized square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defines a class called square"""
+
+
+class Square:
+    """defines the class square"""
+
+    def __init__(self, size):
+        """Initialize a square.
+
+        Args:
+        size (int): len of square
+        """
+        self.__size = size
