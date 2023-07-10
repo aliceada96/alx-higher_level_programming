4732768dc2d6ccb5f94cb8e9f8faebb23a75a113
ALICE ADA AWUOR <aliceada96@gmail.com>
2023-07-10 22:18:22 +0300

    full rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""defines a child Retangle class: inherits from BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Defines a retangle using BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Initialize a rectangle
+
+        Args:
+            width (int): width of rectangle
+            height (int): height of rectangle
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Return area of rectangle"""
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """print() prints str() a description of the rectangle"""
+        string = "[" + str(self.__class.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return (string)
