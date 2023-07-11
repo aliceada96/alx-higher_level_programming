5c12595e3b7032fe192ea822f1fd809e2562ddd1
ALICE ADA AWUOR <aliceada96@gmail.com>
2023-07-11 16:27:36 +0300

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""Defines a function that reads contents of a file"""
+
+
 def read_file(filename=""):
+    """Returns read contents of a file
+
+    Args:
+        filename: Name of file whose contents are to be read
+    """
     with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
