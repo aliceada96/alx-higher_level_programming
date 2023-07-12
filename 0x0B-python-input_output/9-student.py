5d28d8c3076e357a47d79cf667cc82f22c3c8f08
ALICE ADA AWUOR <aliceada96@gmail.com>
2023-07-12 12:43:46 +0300

    edited vars

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -13,7 +13,7 @@ class Student:
             age (int): student age
         """
         self.first_name = first_name
-        self.lats_name = last_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self):
