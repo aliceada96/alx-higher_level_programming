873f3f2f8e0178c2518b1c51fc10c2157d2c518b
aliceada96 <aliceada96@gmail.com>
2023-09-12 22:09:38 +0300

    moby

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
-function executeXTimes (x, theFunction) {
-  for (let i = 0; i < x; i++) {
-    theFunction();
+exports.callMeMoby = function (x, theFunction) {
+  if (typeof theFunction === 'function') {
+    // theFunction is a function, so it's safe to call it
+    for (let i = x; i > 0; i--) {
+      theFunction();
+    }
   }
-}
-
-module.exports = executeXTimes;
+};
