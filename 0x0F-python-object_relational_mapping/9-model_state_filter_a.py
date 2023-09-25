5a18ba4b80634f255f3a60321b1b692274cd93fb
aliceada96 <aliceada96@gmail.com>
2023-09-25 11:09:15 +0300

    filter a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa.
+"""
+
+if __name__ == "__main__":
+
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
+    args = sys.argv
+    mysql_username = args[1]
+    mysql_password = args[2]
+    database_name = args[3]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            mysql_username, mysql_password, database_name
+        ),
+        pool_pre_ping=True,
+    )
+    # create session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = (
+        session.query(State)
+        .filter(State.name.ilike("%a%"))
+        .order_by(State.id.asc())
+        .all()
+    )
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
