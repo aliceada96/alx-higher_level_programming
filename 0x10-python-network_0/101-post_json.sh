e82d7bfb66368496146adefd2704aacc949e7665
aliceada96 <aliceada96@gmail.com>
2023-09-29 00:42:07 +0300

    update2

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a Json post request, URL as argument, displays response body
-curl -X -s -H "Content-Type: application/json" -d @"$2" "$1"
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
